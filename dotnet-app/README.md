74809f187abdf75b691e158a058e965355d2ca2e
shalvah <diakon.ng@gmail.com>
2019-12-19 10:47:06 +0100

    Add .NET app

---
+++ dotnet-app/README.md
@@ -0,0 +1,3 @@
+# Microsoft eShopOnWeb ASP.NET Core Reference Application
+
+ASP.NET MVC sample app for use with Elastic Stack. Modified version of https://github.com/dotnet-architecture/eShopOnWeb.
---
+++ dotnet-app/src/Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile("../../logs/dotnet-app-{Date}.log");
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
